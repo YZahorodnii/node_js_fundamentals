3e18efe69daf89266d0186d220fa20b82fbb830f
YZahorodnii <yzagorodniy@gmail.com>
2023-06-01 22:25:22 +0100

    Homework #2 lesson #2

--- app.js
+++ app.js
@@ -1,122 +1,93 @@
-//
-// // const events = require('events');
-// // const eventEmitter = new events()
-// //
-// // eventEmitter.on('click', ()=> {
-// //     console.log('Event was clicked');
-// // })
-// //
-// // eventEmitter.emit('click');
-// // eventEmitter.emit('click');
-// // eventEmitter.emit('click');
-// // eventEmitter.emit('click');
-// // eventEmitter.emit('click');
-// //
-// //
-// // eventEmitter.once('Clicked and died event', ()=> {
-// //     console.log('Clicked and died event');
-// // });
-// //
-// //
-// // eventEmitter.emit('Clicked and died event');
-// // eventEmitter.emit('Clicked and died event');
-// // eventEmitter.emit('Clicked and died event');
-// // eventEmitter.emit('Clicked and died event');
-// // eventEmitter.emit('Clicked and died event');
-//
-//
-//
-// // const fs = require('fs');
-// // const path = require('path');
-// //
-// // const readStream = fs.createReadStream('file.txt', {highWaterMark: 20 * 1024});
-// // const writeStream = fs.createWriteStream('file2.txt');
-// // readStream.on('data', (chunk) => {
-// //     console.log(chunk);
-// // })
-// //
-// // readStream
-// //     .on('error', (err) => {
-// //         readStream.destroy();
-// //         writeStream.end('Failed to read file')
-// //     })
-// //     .pipe(writeStream)
-//
-// const express = require('express');
-// const app = express();
-//
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}))
-//
-// const users = [
-//     {
-//         name: 'Oleh',
-//         age:2,
-//         gender: 'male'
-//     },
-//     {
-//         name: 'Olya',
-//         age:2,
-//         gender: 'female'
-//     },
-//     {
-//         name: 'Denis',
-//         age:2,
-//         gender: 'male'
-//     },
-//     {
-//         name: 'Kulya',
-//         age:2,
-//         gender: 'female'
-//     }
-// ]
-//
-// app.get('/users', (req, res) => {
-//     res.status(200).json(users)
-// })
-//
-// app.get('/users/:id', (req, res) => {
-//     const {id} = req.params;
-//     res.status(200).json(users[+id]);
-// });
-//
-// app.post('/users', (req, res) => {
-//     users.push(req.body)
-//     res.status(201).json({
-//         message: 'user created'
-//     })
-// })
-//
-// app.put('/users/:id', (req, res) => {
-//     const {id} = req.params;
-//     users[+id] = req.body
-//     res.status(200).json({
-//         message: 'user updated',
-//         data: users[+id]
-//     })
-// });
-//
-// app.delete('/users/:id', (req, res) => {
-//     const {id} = req.params;
-//     users.splice(+id, 1);
-//     res.status(200).json({
-//         message: 'user deleted',
-//         data: users[+id]
-//     })
-// })
-//
-// const PORT = 5001;
-//
-// app.listen(PORT, () => {
-//     console.log(`server has started on PORT ${PORT} `);
-// });
-
 
 //Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs, при видаленні - видаляти
 // При створенні валідацію на імія і вік, імя повинно бути більше 3 символів, вік – не менше нуля
 // На гет, пут, деліт юзерів перевірити чи такий юзер є
 
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 const path = require('path');
 const fs = require('fs');
 
-fs.
+const newUser = {
+    name: 'Zoya',
+    age: 60,
+    gender: 'female'
+};
+
+
+app.post('/users', (req, res) => {
+    const data = fs.readFileSync('base.json', {encoding: "utf-8"});
+    const base = JSON.parse(data);
+    const newUser = req.body
+    if (newUser.name.length > 3 && newUser.age > 0) {
+        base.push(req.body);
+        fs.writeFileSync('base.json', JSON.stringify(base), err => {
+            if (err) throw err;
+        });
+        res.status(200).json({
+            message: 'added new user'
+        })
+    } else {
+        res.status(400).json({
+            message: 'name of the user cannot be less than 3 words and age have to be more than 0'
+        })
+    }
+});
+
+app.get('/users', (req, res) => {
+    const data = fs.readFileSync('base.json', {encoding: "utf-8"});
+    const base = JSON.parse(data);
+    res.json(base)
+});
+
+app.delete('/users/:id', (req, res) => {
+    const data = fs.readFileSync('base.json', {encoding: "utf-8"});
+    const base = JSON.parse(data);
+    let {id} = req.params;
+    if (base[+id]) {
+        base.splice(+id, 1);
+        res.status(200).json({
+            message: 'Deleted user successfully',
+            userDeleted: base[+id]
+        })
+        fs.writeFileSync('base.json', JSON.stringify(base), err => {
+            if (err) throw err;
+        });
+    } else {
+        res.status(404).json({
+            message: 'Cant find user with this id'
+        })
+    }
+})
+
+app.put('/users/:id', (req, res) => {
+    const data = fs.readFileSync('base.json', {encoding: "utf-8"});
+    const base = JSON.parse(data);
+    const {id} = req.params;
+    if (base[+id]) {
+        base[+id] = req.body;
+        res.status(200).json({
+            message: 'Updated user successfully',
+            userUpdated: base[+id]
+        })
+        fs.writeFileSync('base.json', JSON.stringify(base), err => {
+            if (err) throw err;
+        });
+    } else {
+        res.status(404).json({
+            message: 'Cant find user with this id'
+        })
+    }
+})
+
+
+const PORT = 5001;
+
+app.listen(PORT, () => {
+    console.log(`server has started on PORT ${PORT} `);
+});
+
