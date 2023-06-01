fb65f4d7e5480c8dfdaac93957d825f14b24d8a9
YZahorodnii <yzagorodniy@gmail.com>
2023-06-01 17:43:12 +0100

    Lesson 2

--- app.js
+++ app.js
@@ -45,6 +45,9 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
 const users = [
     {
         name: 'Oleh',
@@ -68,10 +71,39 @@ const users = [
     }
 ]
 
+app.get('/users', (req, res) => {
+    res.status(200).json(users)
+})
+
 app.get('/users/:id', (req, res) => {
     const {id} = req.params;
     res.status(200).json(users[+id]);
 });
+
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    res.status(201).json({
+        message: 'user created'
+    })
+})
+
+app.put('/users/:id', (req, res) => {
+    const {id} = req.params;
+    users[+id] = req.body
+    res.status(200).json({
+        message: 'user updated',
+        data: users[+id]
+    })
+});
+
+app.delete('/users/:id', (req, res) => {
+    const {id} = req.params;
+    users.splice(+id, 1);
+    res.status(200).json({
+        message: 'user deleted',
+        data: users[+id]
+    })
+})
 
 const PORT = 5001;
 
