ce841a83aeb6b709c0ebb41749048c13e0f6cacf
YZahorodnii <yzagorodniy@gmail.com>
2023-06-01 18:00:54 +0100

    Homework #2 lesson #2

--- app.js
+++ app.js
@@ -1,112 +1,122 @@
-
-// const events = require('events');
-// const eventEmitter = new events()
 //
-// eventEmitter.on('click', ()=> {
-//     console.log('Event was clicked');
+// // const events = require('events');
+// // const eventEmitter = new events()
+// //
+// // eventEmitter.on('click', ()=> {
+// //     console.log('Event was clicked');
+// // })
+// //
+// // eventEmitter.emit('click');
+// // eventEmitter.emit('click');
+// // eventEmitter.emit('click');
+// // eventEmitter.emit('click');
+// // eventEmitter.emit('click');
+// //
+// //
+// // eventEmitter.once('Clicked and died event', ()=> {
+// //     console.log('Clicked and died event');
+// // });
+// //
+// //
+// // eventEmitter.emit('Clicked and died event');
+// // eventEmitter.emit('Clicked and died event');
+// // eventEmitter.emit('Clicked and died event');
+// // eventEmitter.emit('Clicked and died event');
+// // eventEmitter.emit('Clicked and died event');
+//
+//
+//
+// // const fs = require('fs');
+// // const path = require('path');
+// //
+// // const readStream = fs.createReadStream('file.txt', {highWaterMark: 20 * 1024});
+// // const writeStream = fs.createWriteStream('file2.txt');
+// // readStream.on('data', (chunk) => {
+// //     console.log(chunk);
+// // })
+// //
+// // readStream
+// //     .on('error', (err) => {
+// //         readStream.destroy();
+// //         writeStream.end('Failed to read file')
+// //     })
+// //     .pipe(writeStream)
+//
+// const express = require('express');
+// const app = express();
+//
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}))
+//
+// const users = [
+//     {
+//         name: 'Oleh',
+//         age:2,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Olya',
+//         age:2,
+//         gender: 'female'
+//     },
+//     {
+//         name: 'Denis',
+//         age:2,
+//         gender: 'male'
+//     },
+//     {
+//         name: 'Kulya',
+//         age:2,
+//         gender: 'female'
+//     }
+// ]
+//
+// app.get('/users', (req, res) => {
+//     res.status(200).json(users)
 // })
 //
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-// eventEmitter.emit('click');
-//
-//
-// eventEmitter.once('Clicked and died event', ()=> {
-//     console.log('Clicked and died event');
+// app.get('/users/:id', (req, res) => {
+//     const {id} = req.params;
+//     res.status(200).json(users[+id]);
 // });
 //
-//
-// eventEmitter.emit('Clicked and died event');
-// eventEmitter.emit('Clicked and died event');
-// eventEmitter.emit('Clicked and died event');
-// eventEmitter.emit('Clicked and died event');
-// eventEmitter.emit('Clicked and died event');
-
-
-
-// const fs = require('fs');
-// const path = require('path');
-//
-// const readStream = fs.createReadStream('file.txt', {highWaterMark: 20 * 1024});
-// const writeStream = fs.createWriteStream('file2.txt');
-// readStream.on('data', (chunk) => {
-//     console.log(chunk);
+// app.post('/users', (req, res) => {
+//     users.push(req.body)
+//     res.status(201).json({
+//         message: 'user created'
+//     })
 // })
 //
-// readStream
-//     .on('error', (err) => {
-//         readStream.destroy();
-//         writeStream.end('Failed to read file')
+// app.put('/users/:id', (req, res) => {
+//     const {id} = req.params;
+//     users[+id] = req.body
+//     res.status(200).json({
+//         message: 'user updated',
+//         data: users[+id]
 //     })
-//     .pipe(writeStream)
+// });
+//
+// app.delete('/users/:id', (req, res) => {
+//     const {id} = req.params;
+//     users.splice(+id, 1);
+//     res.status(200).json({
+//         message: 'user deleted',
+//         data: users[+id]
+//     })
+// })
+//
+// const PORT = 5001;
+//
+// app.listen(PORT, () => {
+//     console.log(`server has started on PORT ${PORT} `);
+// });
 
-const express = require('express');
-const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+//Винести базу даних в json.file, при створенні записувати туда нових юзерів через fs, при видаленні - видаляти
+// При створенні валідацію на імія і вік, імя повинно бути більше 3 символів, вік – не менше нуля
+// На гет, пут, деліт юзерів перевірити чи такий юзер є
 
-const users = [
-    {
-        name: 'Oleh',
-        age:2,
-        gender: 'male'
-    },
-    {
-        name: 'Olya',
-        age:2,
-        gender: 'female'
-    },
-    {
-        name: 'Denis',
-        age:2,
-        gender: 'male'
-    },
-    {
-        name: 'Kulya',
-        age:2,
-        gender: 'female'
-    }
-]
+const path = require('path');
+const fs = require('fs');
 
-app.get('/users', (req, res) => {
-    res.status(200).json(users)
-})
-
-app.get('/users/:id', (req, res) => {
-    const {id} = req.params;
-    res.status(200).json(users[+id]);
-});
-
-app.post('/users', (req, res) => {
-    users.push(req.body)
-    res.status(201).json({
-        message: 'user created'
-    })
-})
-
-app.put('/users/:id', (req, res) => {
-    const {id} = req.params;
-    users[+id] = req.body
-    res.status(200).json({
-        message: 'user updated',
-        data: users[+id]
-    })
-});
-
-app.delete('/users/:id', (req, res) => {
-    const {id} = req.params;
-    users.splice(+id, 1);
-    res.status(200).json({
-        message: 'user deleted',
-        data: users[+id]
-    })
-})
-
-const PORT = 5001;
-
-app.listen(PORT, () => {
-    console.log(`server has started on PORT ${PORT} `);
-});
+fs.
